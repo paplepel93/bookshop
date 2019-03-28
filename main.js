c2b409d5961d6efd2a9eafd0040f4a40760d01a0
paplepel93 <47503656+paplepel93@users.noreply.github.com>
2019-03-28 16:32:39 +0100

    added bucnh of events

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
 function eventlist() {
     document.getElementById("logbutton").addEventListener("click", loggedin);
     document.getElementById("logout").addEventListener("click", loggedout);
+    document.getElementById("okbutton").addEventListener("click", search);
+    document.getElementById("regbut").addEventListener("click", register);
+}
+
+function register() {
+    window.alert();
+    /* Deze functie wordt aangeroepen als iemand op de register knop drukt */
+    var regname = document.getElementById("inputname").value;
+    var regage = document.getElementById("inputage").value;
+    var regadres = document.getElementById("inputadres").value;
+    var regcountry = document.getElementById("inputcountry").value;
+    var regemail = document.getElementById("regemail").value;
+    var regusername = document.getElementById("inputusername").value;
+    var regpas1 = document.getElementById("inputpas1").value;
+    var regpas2 = document.getElementById("inputpas2").value;
+
+    if (regpas1 !== regpas2) {
+        window.alert("Passwords do not match try again");
+        document.getElementById("inputpas1").value = "";
+        document.getElementById("inputpas2").value = "";
+    }
+
+    var totaluser = [regname, regage, regadres, regcountry, regemail, regusername, regpas1, regpas2]
+    var L = totaluser.length;
+    
+    for (var u = 0; u < L; u++) {
+        if (totaluser[u].toString() == "")
+        {
+            window.alert("You have not completely filled out the form, Please make sure you have filled in " + totaluser[u])
+        }
+    }
+
+
+
+}
+
+function search() {
+    /* Deze functie wordt aangeroepen als iemand op de zoekknop naast de zoekbar drukt*/
+    var searchterm = document.getElementById("searchbar").value;
+    window.alert("you have searched for: " + searchterm);
 }
 
 function loggedin() {
@@ -8,7 +48,7 @@ function loggedin() {
     var inputname = document.getElementById("input1").value;
     var inputpas = document.getElementById("input2").value;
     /* giving feedback about the logging in */
-    window.alert("You logged in as " + inputname);
+    window.alert("You logged in as: " + inputname);
     /* change the page accordingly */
     document.getElementById("loggedin1").textContent += inputname;
     document.getElementById("username1").textContent += inputname;
